1d2e0ce6c43b835f467ba16ad72e49ca46eb6e96
CarolPatricio <carolpatricio28@hotmail.com>
2021-09-02 20:26:05 -0300

    Ler arquivo passado

--- src/SnakeGame.cpp
+++ src/SnakeGame.cpp
@@ -1,4 +1,4 @@
-#include "SnakeGame.h"
+#include "../include/SnakeGame.h"
 
 #include <iostream>
 #include <fstream>
@@ -8,20 +8,25 @@
 
 using namespace std;
 
-SnakeGame::SnakeGame(){
+SnakeGame::SnakeGame()
+{
     choice = "";
     frameCount = 0;
     initialize_game();
 }
 
-void SnakeGame::initialize_game(){
+void SnakeGame::initialize_game()
+{
     //carrega o nivel ou os níveis
     ifstream levelFile("data/maze1.txt"); //só dá certo se o jogo for executado dentro da raíz do diretório (vc vai resolver esse problema pegando o arquivo da linha de comando)
     int lineCount = 0;
     string line;
-    if(levelFile.is_open()){
-        while(getline(levelFile, line)){ //pega cada linha do arquivo
-            if(lineCount > 0){ //ignora a primeira linha já que ela contem informações que não são uteis para esse exemplo
+    if (levelFile.is_open())
+    {
+        while (getline(levelFile, line))
+        { //pega cada linha do arquivo
+            if (lineCount > 0)
+            { //ignora a primeira linha já que ela contem informações que não são uteis para esse exemplo
                 maze.push_back(line);
             }
             lineCount++;
@@ -30,43 +35,47 @@ void SnakeGame::initialize_game(){
     state = RUNNING;
 }
 
-
-
-void SnakeGame::process_actions(){
+void SnakeGame::process_actions()
+{
     //processa as entradas do jogador de acordo com o estado do jogo
     //nesse exemplo o jogo tem 3 estados, WAITING_USER, RUNNING e GAME_OVER.
     //no caso deste trabalho temos 2 tipos de entrada, uma que vem da classe Player, como resultado do processamento da IA
     //outra vem do próprio usuário na forma de uma entrada do teclado.
-    switch(state){
-        case WAITING_USER: //o jogo bloqueia aqui esperando o usuário digitar a escolha dele
-            cin>>std::ws>>choice;
-            break;
-        default:
-            //nada pra fazer aqui
-            break;
+    switch (state)
+    {
+    case WAITING_USER: //o jogo bloqueia aqui esperando o usuário digitar a escolha dele
+        cin >> std::ws >> choice;
+        break;
+    default:
+        //nada pra fazer aqui
+        break;
     }
 }
 
-void SnakeGame::update(){
+void SnakeGame::update()
+{
     //atualiza o estado do jogo de acordo com o resultado da chamada de "process_input"
-    switch(state){
-        case RUNNING:
-            if(frameCount>0 && frameCount%10 == 0) //depois de 10 frames o jogo pergunta se o usuário quer continuar
-                state = WAITING_USER;
-            break;
-        case WAITING_USER: //se o jogo estava esperando pelo usuário então ele testa qual a escolha que foi feita
-            if(choice == "n"){
-                state = GAME_OVER;
-                game_over();
-            }
-            else{
-                //pode fazer alguma coisa antes de fazer isso aqui
-                state = RUNNING;
-            }
-            break;
-        default:
-            //nada pra fazer aqui
-            break;
+    switch (state)
+    {
+    case RUNNING:
+        if (frameCount > 0 && frameCount % 10 == 0) //depois de 10 frames o jogo pergunta se o usuário quer continuar
+            state = WAITING_USER;
+        break;
+    case WAITING_USER: //se o jogo estava esperando pelo usuário então ele testa qual a escolha que foi feita
+        if (choice == "n")
+        {
+            state = GAME_OVER;
+            game_over();
+        }
+        else
+        {
+            //pode fazer alguma coisa antes de fazer isso aqui
+            state = RUNNING;
+        }
+        break;
+    default:
+        //nada pra fazer aqui
+        break;
     }
 }
 
@@ -74,51 +83,59 @@ void SnakeGame::update(){
  * @brief função auxiliar para fazer o programa esperar por alguns milisegundos
  * @param ms a quantidade de segundos que o programa deve esperar
  */
-void wait(int ms){
+void wait(int ms)
+{
     this_thread::sleep_for(chrono::milliseconds(ms));
 }
 
 /**
  * @brief função auxiliar para limpar o terminal
  */
-void clearScreen(){
+void clearScreen()
+{
 //some C++ voodoo here ;D
 #if defined _WIN32
     system("cls");
-#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+#elif defined(__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
     system("clear");
-#elif defined (__APPLE__)
+#elif defined(__APPLE__)
     system("clear");
 #endif
 }
 
-void SnakeGame::render(){
+void SnakeGame::render()
+{
     clearScreen();
-    switch(state){
-        case RUNNING:
-            //desenha todas as linhas do labirinto
-            for(auto line : maze){
-                cout<<line<<endl;
-            }
-            break;
-        case WAITING_USER:
-            cout<<"Você quer continuar com o jogo? (s/n)"<<endl;
-            break;
-        case GAME_OVER:
-            cout<<"O jogo terminou!"<<endl;
-            break;
+    switch (state)
+    {
+    case RUNNING:
+        //desenha todas as linhas do labirinto
+        for (auto line : maze)
+        {
+            cout << line << endl;
+        }
+        break;
+    case WAITING_USER:
+        cout << "Você quer continuar com o jogo? (s/n)" << endl;
+        break;
+    case GAME_OVER:
+        cout << "O jogo terminou!" << endl;
+        break;
     }
     frameCount++;
 }
 
-void SnakeGame::game_over(){
+void SnakeGame::game_over()
+{
 }
 
-void SnakeGame::loop(){
-    while(state != GAME_OVER){
+void SnakeGame::loop()
+{
+    while (state != GAME_OVER)
+    {
         process_actions();
         update();
         render();
-        wait(1000);// espera 1 segundo entre cada frame
+        wait(1000); // espera 1 segundo entre cada frame
     }
 }
--- src/Snaze.cpp
+++ src/Snaze.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
-#include "SnakeGame.h"
+#include "../include/SnakeGame.h"
 
 using namespace std;
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "[ E ]: Forneca o caminho completo do arquivo por meio do parametro. \n");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *arquivo = fopen(argv[1], "r");
+
+    if (arquivo == NULL)
+    {
+        fprintf(stderr, "[ E ]: Arquivo nao encontrado. \n");
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(arquivo);
+    printf("[I]: Sucesso. \n");
+    return EXIT_SUCCESS;
     SnakeGame game;
-    
+
     game.loop(); //bloqueia aqui e só saí quando o jogo termina
 
     return 0;
